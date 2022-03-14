959d42e2cefcf1ae4f54134f74e9d64c18ed0649
Piotr Szmil <szmilekk007@wp.pl>
2022-03-14 13:12:09 +0100

    Create simple login panel without returning data

--- src/components/LoginPanel.vue
+++ src/components/LoginPanel.vue
@@ -1,3 +1,15 @@
 <template>
-    
+
+<div class="Loginpanel">
+    <form action="">  //Trzeba zwrócić wpisaną wartość login i hasło do obsługi
+
+        <label for="login">LOGIN:</label><br>
+        <input type="text" id="login" name="login" value=""><br>
+        <label for="password">HASŁO:</label><br>
+        <input type="text" id="password" name="password" value=""><br><br>
+        <input type="submit" value="Submit">
+
+    </form>
+</div>
+
 </template>
