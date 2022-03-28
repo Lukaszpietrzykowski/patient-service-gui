aba359454aa7838877d3d358631f1414fd4b71d0
Radoslaw Tomaszewski <radek4441399@gmail.com>
2022-03-28 16:46:30 +0200

    Panel Login changes

--- src/components/LoginPanel.vue
+++ src/components/LoginPanel.vue
@@ -1,27 +1,120 @@
 <template>
 
-<section class="bg-white d-flex justify-content-center vh-100 w-100 align-items-center flex-column flex-md-row-reverse">
+<section>
 
-    <div class="w-100 d-flex justify-content-center align-items-center h-100">
-        <img src="../assets/logo.png" class="img-fluid logo">
+<div id="container">
+    <div id="left">
+        <div id="image"></div>
     </div>
-    
-    <div class="w-100 d-flex justify-content-center align-items-center h-100 bg-dark-red flex-column">
-        <h1 class="font-medium color-white font-weight-bold text-center text-lg-start w-75 mb-1">Witamy!</h1>
-        <h2 class="font-small color-white font-weight-bold text-center text-lg-start w-75 mb-4">Zaloguj się.</h2>
-        <form class="d-flex justify-content-center align-items-center flex-column flex-lg-row w-75 w-lg-75" method="POST" action="">
-            
-            <div class="mr-2 w-100 h-100">
-                <input class="form-control mb-2" type="email" id="login" name="login" placeholder="Login" required>
-                
-                <input class="form-control mt-2" type="password" id="password" name="password" placeholder="Password" required>
-            </div>
-                
-                
-            <button class="btn btn-black mx-2 w-100 h-100 mt-3 mt-lg-0 w-lg-50" type="submit" name="submit">Log In</button>
-
+    <div id="right">
+        <div id="login">
+        <h1>Dzień dobry.</h1>
+        <p>Wprowadź login i hasło.</p>
+        <form>
+            <input type="text" placeholder="login" onfocus="this.placeholder=''" onblur="this.placeholder='login'"><br/>
+            <input type="password" placeholder="hasło" onfocus="this.placeholder=''" onblur="this.placeholder='hasło'"><br/>
+            <input type="submit" value="Zaloguj się">
         </form>
+        </div>
     </div>
+</div>
 </section>
 
 </template>
+
+<style>
+/*do uzupełnienia:
+- logo SOP
+- napis obok logo nad szpitalem SYSTEM OBSŁUGI PACJENTÓW
+*/
+#container{
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+#left{
+    float:left;
+    width: 50%;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    background-color: #00bfba;
+    text-align: left;
+    z-index: 1;
+    box-shadow: 5px 5px 10px;
+}
+#right{
+    float: right;
+    width: 50%;
+    height: 100vh;
+    background-color: #3075ff;
+    margin: 0 auto;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+	align-items: center;
+    z-index: 2;
+    box-shadow: 10px 10px 5px lightblue;
+}
+#image {
+    width: 100%;
+    height: 100%;
+    background: url('../assets/img02.svg') no-repeat center;
+    background-size: 80%;
+}
+#login
+{
+    text-align: left;
+    margin: 0 auto;
+    background-color: #00bfba;
+    width: 75%;
+    padding: 5%;
+    border-radius: 15px;
+    -webkit-box-shadow: 10px 9px 9px 0px rgba(66, 68, 90, 1);
+    -moz-box-shadow: 10px 9px 9px 0px rgba(66, 68, 90, 1);
+    box-shadow: 10px 9px 9px 0px rgba(66, 68, 90, 1);
+}
+input[type=text],
+input[type=password]
+{
+    width: 100%;
+    background-color: #efefef;
+    border: 2px solid #ddd;
+    border-radius: 5px;
+    padding: 2%;
+    box-sizing: border-box;
+    outline: none; /* wyłączenie czarnej obwódki */
+    margin-top: 2%;
+}
+input[type=text]:focus,
+input[type=password]:focus
+{
+    border: 2px solid #077262;
+    background-color: ##bdbdbd;
+}
+input[type=submit]
+{
+    width: 100%;
+    background-color: #077262;
+    color: white;
+    padding: 2%;
+    margin-top: 3%;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    letter-spacing: 3%;
+    outline: none;
+}
+input[type=submit]:hover
+{
+    background-color: #055040;
+}
+input::-webkit-input-placeholder
+{
+    color:#999;
+}
+p{
+    font-size: x-large;
+}
+</style>
