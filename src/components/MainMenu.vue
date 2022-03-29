0c5215b2bc1f093904df4c9dea3d74268986564a
Piotr Szmil <szmilekk007@wp.pl>
2022-03-29 20:19:55 +0200

    added correction to main menu

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <section class="bg-white d-flex flex-column align-items-center justify-content-center">
+    <section class="bg-white position-relative d-flex flex-column align-items-center justify-content-center">
         
         <header class="d-flex flex-column justify-content-center w-100 p-3 " style="background: #05a081; box-shadow: 2px 2px 1em grey; border-radius: 0 0 6rem 6rem; width:1000px;">
             <h1 class="font-medium color-white font-weight-bold text-center mt-1 p-2">Witaj!</h1>
@@ -14,9 +14,9 @@
             </form>
         </header>
 
-        <section class="fixed-bottom d-flex flex-row justify-content-center align-items-center w-75 p-2" style="gap: 3rem; background: #05a081; box-shadow: -2px -2px 1em grey; border-radius: 6rem 6rem 0px 0px; width:1000px; margin-right:auto; margin-left: auto;">
-            <div>
-                <section class="d-flex justify-content-center align-items-center flex-column w-75" style="background-color: #D4D9DB;padding:3%; border-radius: 0pc 0pc 1pc 1pc; box-shadow: 0 0.46875rem 2.1875rem rgb(90 97 105 / 10%), 0 0.9375rem 1.40625rem rgb(90 97 105 / 10%), 0 0.25rem 0.53125rem rgb(90 97 105 / 12%), 0 0.125rem 0.1875rem rgb(90 97 105 / 10%);">
+        
+            
+                <section class="d-flex justify-content-center align-items-center flex-column w-50 mt-1 mb-2" style="background-color: #D4D9DB;padding:3%; border-radius: 1pc 1pc 1pc 1pc; box-shadow: 0 0.46875rem 2.1875rem rgb(90 97 105 / 10%), 0 0.9375rem 1.40625rem rgb(90 97 105 / 10%), 0 0.25rem 0.53125rem rgb(90 97 105 / 12%), 0 0.125rem 0.1875rem rgb(90 97 105 / 10%);">
                     <div class="" style="color: black;font-weight:bold; padding-bottom:1rem;">Nazwa szpitala</div>
                     <div class="" style="color: black;padding-bottom:2rem;">-</div> 
                     <div class="" style="color: black;font-weight:bold;padding-bottom:1rem;"> Miejscowość </div> 
@@ -25,14 +25,16 @@
                     <div class="" style="color: black;padding-bottom:2rem;float:left;"> - </div> 
                     <div class="" style="color: black;float:left; font-weight:bold;padding-bottom:1rem;"> Oddziały </div>
                     <div class="" style="width:30%;color: black;float:left;">
-                    <div class="" style="float:left; width:50%;">-</div>
-                    <div class="" style="float:left; width:50%;"><svg width="15px" style="margin-left:50px" fill="green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"/></svg>
-                    </div>
+
+                        <div class="" style="float:left; width:50%;">-</div>
+                        <div class="" style="float:left; width:50%;"><svg width="15px" style="margin-left:50px" fill="green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"/></svg>
+                        </div>
                     </div>
                 </section>
-            </div>
-        </section>
-        <section class="fixed-bottom d-flex flex-row justify-content-center align-items-center w-75" style="gap: 3rem; background-color: #05a081; box-shadow: -2px -2px 1em grey; border-radius: 60px 60px 0px 0px; height:60px; width:1000px; margin-right:auto; margin-left: auto;">
+            
+      
+        
+        <section class="fixed-bottom d-flex flex-row justify-content-center align-items-center w-75 p-2" style="gap: 4rem; background-color: #05a081; box-shadow: -2px -2px 1em grey; border-radius: 60px 60px 0px 0px; width:1000px; margin-right:auto; margin-left: auto;">
               
                 <div class="d-flex flex-column justify-content-center">
                     <a href="" class=" mt-2" style="background: transparent; border-radius: 10pc 10pc 10pc 10pc; width:45px; height:45px;"> <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M192 48C192 21.49 213.5 0 240 0H400C426.5 0 448 21.49 448 48V512H368V432C368 405.5 346.5 384 320 384C293.5 384 272 405.5 272 432V512H192V48zM312 64C303.2 64 296 71.16 296 80V104H272C263.2 104 256 111.2 256 120V136C256 144.8 263.2 152 272 152H296V176C296 184.8 303.2 192 312 192H328C336.8 192 344 184.8 344 176V152H368C376.8 152 384 144.8 384 136V120C384 111.2 376.8 104 368 104H344V80C344 71.16 336.8 64 328 64H312zM160 96V512H48C21.49 512 0 490.5 0 464V320H80C88.84 320 96 312.8 96 304C96 295.2 88.84 288 80 288H0V224H80C88.84 224 96 216.8 96 208C96 199.2 88.84 192 80 192H0V144C0 117.5 21.49 96 48 96H160zM592 96C618.5 96 640 117.5 640 144V192H560C551.2 192 544 199.2 544 208C544 216.8 551.2 224 560 224H640V288H560C551.2 288 544 295.2 544 304C544 312.8 551.2 320 560 320H640V464C640 490.5 618.5 512 592 512H480V96H592z"/></svg> </a>
