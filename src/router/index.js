f5ef81a82c2535b5910d075b6c141cba69589bf4
Piotr Szmil <szmilekk007@wp.pl>
2022-04-07 12:29:30 +0200

    Added corrections to Main Menu panel and links to router for menu and login panels

--- src/router/index.js
+++ src/router/index.js
@@ -2,31 +2,39 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginPanel from '../components/LoginPanel.vue'
 import style from '../scss/style.scss'
+import MainMenu from '../components/MainMenu.vue'
 
-const routes = [
-  {
-    path: '/login',
-    name: 'loginpage',
-    component: LoginPanel, style
-  },
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+const routes = [{
+        path: '/login',
+        name: 'loginpage',
+        component: LoginPanel,
+        style
+    },
+    {
+        path: '/hospitalsview',
+        name: 'menu',
+        component: MainMenu,
+        style
+    },
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/AboutView.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
