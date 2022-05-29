091933681eb607c74ea086d42228a12b6bb337a5
LPIETRZYKOWSKI <lukasz.pietrzykowski12@gmail.com>
2022-05-29 13:29:32 +0200

    poprawka przekierowań

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
-  <nav class="d-none">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/hospitalsview">Mainmenu</router-link> |
-    <router-link to="/login">Login panel</router-link>
-  </nav>
-  <router-view></router-view>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,16 +1,16 @@
 <template>
 
     <section class="bg-white position-relative d-flex flex-column align-items-center justify-content-center h-100">
-        
+
         <header class="d-flex flex-column justify-content-center w-100 bg-for-menu bg-main menu-header p-3 position-fixed top-0">
             <form class="d-flex justify-content-center w-100">
 
                 <div class="w-75 w-lg-50 d-flex flex-row justify-content-center">
                     <input class="input-search h-100 p-0 m-0 outline-none border-0 " type="text" placeholder="Wyszukaj szpital" v-model="search">
-                </div> 
+                </div>
 
             </form>
-            
+
         </header>
 
         <div class="d-flex flex-column justify-content-center menu-logout">
@@ -32,7 +32,7 @@
 
         <div class="mb-5" v-if="hospitals.length">
             <div  v-for="hospital_one of hospitals" v-bind:key="hospital_one.name" class="d-flex p-3 my-3 w-100 w-lg-75 mx-auto justify-content-center flex-column flex-md-row menu-hospital-box">
-                
+
                     <div class="w-100 py-1 p-lg-3">
                         <div class="w-100 d-flex flex-row p-2 justify-content-center align-items-center" style="gap:0.75rem">
                             <svg width="30" fill="#05a081" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM368 360c0 13.25-10.75 24-24 24S320 373.3 320 360v-80H192v80C192 373.3 181.3 384 168 384S144 373.3 144 360v-208C144 138.8 154.8 128 168 128S192 138.8 192 152v80h128v-80C320 138.8 330.8 128 344 128s24 10.75 24 24V360z"/></svg>
@@ -73,40 +73,45 @@
            <!-- nutrient in food.nutrients -->
            <!-- {{department.name}} -->
        </ul>
-        
+
         </div>
 
-      
-        
         <section class="fixed-bottom d-flex flex-row justify-content-center align-items-center w-100 p-1 bg-transparent">
-            
-            <div class="bg-for-menu bg-main justify-content-center align-items-center d-flex flex-row menu-fixed-bottom-inner px-4 w-max-content">
-                <div class="d-flex flex-column justify-content-center">
-                    <a href="" class="bg-transparent mt-2">
-                        <svg class="menu-fixed-button" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M192 48C192 21.49 213.5 0 240 0H400C426.5 0 448 21.49 448 48V512H368V432C368 405.5 346.5 384 320 384C293.5 384 272 405.5 272 432V512H192V48zM312 64C303.2 64 296 71.16 296 80V104H272C263.2 104 256 111.2 256 120V136C256 144.8 263.2 152 272 152H296V176C296 184.8 303.2 192 312 192H328C336.8 192 344 184.8 344 176V152H368C376.8 152 384 144.8 384 136V120C384 111.2 376.8 104 368 104H344V80C344 71.16 336.8 64 328 64H312zM160 96V512H48C21.49 512 0 490.5 0 464V320H80C88.84 320 96 312.8 96 304C96 295.2 88.84 288 80 288H0V224H80C88.84 224 96 216.8 96 208C96 199.2 88.84 192 80 192H0V144C0 117.5 21.49 96 48 96H160zM592 96C618.5 96 640 117.5 640 144V192H560C551.2 192 544 199.2 544 208C544 216.8 551.2 224 560 224H640V288H560C551.2 288 544 295.2 544 304C544 312.8 551.2 320 560 320H640V464C640 490.5 618.5 512 592 512H480V96H592z"/></svg>
-                    </a>
-                    <h2 class="font-small color-white font-weight-normal text-center " style="font-size: 0.75rem; letter-spacing: 0.5px">Dodaj szpital</h2>
-                </div>
-    
+            <div  class="bg-for-menu bg-main justify-content-center align-items-center d-flex flex-row menu-fixed-bottom-inner px-4 w-max-content">
                 <div class="d-flex flex-column justify-content-center">
 
-                   <router-link to="/patients"  class="bg-transparent mt-2"> 
-                        <svg class="menu-fixed-button" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224 256c70.7 0 128-57.31 128-128S294.7 0 224 0C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3C0 496.5 15.52 512 34.66 512h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM616 200h-48v-48C568 138.8 557.3 128 544 128s-24 10.75-24 24v48h-48C458.8 200 448 210.8 448 224s10.75 24 24 24h48v48C520 309.3 530.8 320 544 320s24-10.75 24-24v-48h48C629.3 248 640 237.3 640 224S629.3 200 616 200z"/></svg>
+                   <router-link to="/patients"  class="bg-transparent mt-2">
+                     <svg class="menu-fixed-button" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"/></svg>
                     </router-link>
-                    
-                    <h2 class="font-small color-white font-weight-normal text-center " style="font-size: 0.75rem; letter-spacing: 0.5px">Dodaj pacjenta</h2>
+
+                    <h2 class="font-small color-white font-weight-normal text-center " style="font-size: 0.75rem; letter-spacing: 0.5px">Pacjenci</h2>
                 </div>
-                
+
+              <div class="d-flex flex-column justify-content-center">
+
+                <router-link to="/adding-patient"  class="bg-transparent mt-2">
+                  <svg class="menu-fixed-button" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224 256c70.7 0 128-57.31 128-128S294.7 0 224 0C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3C0 496.5 15.52 512 34.66 512h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM616 200h-48v-48C568 138.8 557.3 128 544 128s-24 10.75-24 24v48h-48C458.8 200 448 210.8 448 224s10.75 24 24 24h48v48C520 309.3 530.8 320 544 320s24-10.75 24-24v-48h48C629.3 248 640 237.3 640 224S629.3 200 616 200z"/></svg>
+                </router-link>
+                <h2 class="font-small color-white font-weight-normal text-center " style="font-size: 0.75rem; letter-spacing: 0.5px">Dodaj pacjenta</h2>
+              </div>
+
             </div>
 
         </section>
     </section>
-       
-       
+
+
 
             <!-- {{hospitals}} -->
 
 </template>
+
+<style>
+  .mb-5 {
+    margin-top: 5rem;
+    margin-bottom: 10px!important;
+  }
+</style>
 
 <script>
 
@@ -129,7 +134,7 @@ export default {
 mounted() {
         axios.get('https://patient-service-api.herokuapp.com/hospital/all')
     .then(response => {
-        
+
     this.hospitals = response.data
     console.log(this.hospitals)
     })
@@ -143,12 +148,5 @@ mounted() {
     }
    }
 }
-  
-
-
- 
-
-// app.use(cors());
-
 
 </script>
