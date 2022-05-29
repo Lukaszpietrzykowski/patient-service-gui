86e20a866e8855da2bb0bc9e89d6e82979cf3173
LPIETRZYKOWSKI <lukasz.pietrzykowski12@gmail.com>
2022-05-29 16:33:30 +0200

    poprawka przekierowań

--- src/components/LoginPanel.vue
+++ src/components/LoginPanel.vue
@@ -1,22 +1,25 @@
 <template>
 
-<section class="d-flex flex-column flex-lg-row vh-100">
+  <section class="d-flex flex-column flex-lg-row vh-100">
     <div class="h-100 w-100 d-flex justify-content-center align-items-center bg-main">
-        <img class="img-fluid p-lg-5 mw-sm-450" src="../assets/hospital_green.svg">
+      <img class="img-fluid p-lg-5 mw-sm-450" src="../assets/hospital_green.svg">
     </div>
     <div class="h-100 w-100 d-flex justify-content-center align-items-center bg-login p-4">
-        <div class="login bg-white">
+      <div class="login bg-white">
         <h1 class="font-medium">Dzień dobry</h1>
         <p class="font-small mb-0 mt-2">Wprowadź login i hasło</p>
         <form>
-            <input class="input-form" type="email" placeholder="login"  name="email" v-model="email" required><br/>
-            <input class="input-form" type="password" placeholder="hasło"   name="password" v-model="password" required><br/>
-                    <button v-on:click="login" class="btn-form btn-form-green p-3 text-decoration-none mt-2">Zaloguj się</button>
+          <input class="input-form" type="text" placeholder="login" name="email" v-model="email" required><br/>
+          <input class="input-form" type="password" placeholder="hasło" name="password" v-model="password"
+                 required><br/>
+          <button v-on:click="login(email, password)" class="btn-form btn-form-green p-3 text-decoration-none mt-2">
+            Zaloguj się
+          </button>
         </form>
         <p v-if="showError" id="error">Email albo hasło jest nieprawidłowe.</p>
-        </div>
+      </div>
     </div>
-</section>
+  </section>
 </template>
 
 <script>
@@ -25,43 +28,55 @@ import axios from 'axios';
 // import cors from 'cors';
 // import app from 'vue';
 export default {
-name: 'LoginPanel',
+  name: 'LoginPanel',
   data() {
     return {
-          email: "",
-          password: ""
+      email: "",
+      password: ""
     }
   },
 
-  methods:{
-      async login() {
-          axios.get(`https://patient-service-api.herokuapp.com/user`)
-        .then(response => {
-            // console.log(response.email)
-            
+  methods: {
+
+    getUserData() {
+      axios.get(`http://patient-service-api.herokuapp.com/user`)
+          .then(response => {
             this.users = response.data
             console.log(response.data)
-                if(response.status === 200 && response.data.length>0) {
-                        localStorage.setItem('user-info', JSON.stringify(response.data))
-                        this.$router.push({name: 'MainMenu'})
+            if (response.status === 200 && response.data.length > 0) {
+              localStorage.setItem('user-info', JSON.stringify(response.data))
+            }
+          })
+    },
+
+    async login(email, password) {
+
+      const params = new URLSearchParams();
+      params.append('username', email);
+      params.append('password', password);
+
+      axios.post('http://patient-service-api.herokuapp.com/login', params, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        }
+      })
+          .then(response => {
+                if (response.status === 200 && response.data.length > 0) {
+                  // this.getUserData();
+                  console.log("elo")
+                  this.$router.push({name: 'MainMenu'})
                 }
-                
+              }
+          )
+    },
+    mounted() {
 
-            
-
-
-    })
-  },
-      mounted() {
-          
-          let user = localStorage.getItem('user-info')
-          if(user) {
-              this.$router.push({name: "MainMenu"})
-          }
+      let user = localStorage.getItem('user-info')
+      if (user) {
+        this.$router.push({name: "MainMenu"})
       }
+    }
   }
 
 }
-
-
 </script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "./scss/style.scss"
+import axios from "axios";
 
 
 // import Vue from 'vue';
 // import VueFilter from 'vue-filter';
 
-
+axios.defaults.withCredentials = true;
 createApp(App).use(router).mount('#app');
 // Vue.use(VueFilter);
