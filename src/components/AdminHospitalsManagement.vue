3a18848385cdc0fbc7e38b7d14b8b63772c10fd3
mvlinowskv <46280152+mvlinowskv@users.noreply.github.com>
2022-06-19 14:00:17 +0200

    add mobile menu

--- src/components/AdminHospitalsManagement.vue
+++ src/components/AdminHospitalsManagement.vue
@@ -123,10 +123,30 @@
     </section>
 
 
-    <section class="fixed-bottom d-flex flex-row justify-content-center align-items-center w-100 p-1 bg-transparent">
-      <div class="bg-for-menu bg-main justify-content-center align-items-center d-flex flex-row gap-0 gap-md-2 gap-lg-3 menu-fixed-bottom-inner px-4 w-max-content">
-        <MenuComponent/>
-        <div class="d-flex flex-column justify-content-center">
+<section class="box-hamburger d-lg-none">
+            <button class="btn btn-form-green btn-menu" v-on:click="openMenu()">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                    <path
+                        d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z" />
+                </svg>
+            </button>
+        </section>
+        <section class="h-100 w-100 menu-box fixed-bottom" id="menu" v-on:click="turnOffMenu()">
+
+            <section class="d-flex flex-column flex-md-row justify-content-end justify-content-md-center align-items-center w-100 p-1 bg-transparent">
+                            <button class="btn btn-form-green btn-cross" v-on:click="turnOffMenu()">
+                <svg fill="#fff" width="35" height="35" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                    <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                    <path
+                        d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
+                    </svg>
+
+            </button>
+                <div
+                    class="bg-for-menu bg-main justify-content-center align-items-center d-flex flex-column gap-0 flex-md-row menu-fixed-bottom-inner gap-0 gap-md-2 gap-lg-3 px-4 w-max-content">
+                    <MenuComponent />
+                              <div class="d-flex flex-column justify-content-center">
           <a href="/#/adding-hospital" class="bg-transparent mt-2">
             <svg class="menu-fixed-button" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
               <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
@@ -137,21 +157,13 @@
           <h2 class="font-small color-white font-weight-normal text-center "
               style="font-size: 0.75rem; letter-spacing: 0.5px">Dodaj szpital</h2>
         </div>
+                </div>
+            </section>
+        </section>
 
-
-
-
-
-
-
-
-
-
-
-      </div>
-
-    </section>
   </section>
+
+
 
 </template>
 
@@ -192,7 +204,19 @@ export default {
 
     managementWards(id) {
       this.$router.push({name: 'AdminWardManagement', params: {id: id}})
-    }
+    },
+                openMenu() {
+                const menu = document.querySelector('#menu')
+
+                menu.style.bottom = "0rem"
+
+
+            },
+            turnOffMenu() {
+                const menu = document.querySelector('#menu')
+
+                menu.style.bottom = "-100vh"
+            }
   },
 
   mounted() {
--- src/main.js
+++ src/main.js
@@ -17,5 +17,9 @@ router.beforeEach((to, from, next) => {
     }
 })
 
+
+
+
+
 axios.defaults.withCredentials = true;
 createApp(App).use(router).mount('#app');
