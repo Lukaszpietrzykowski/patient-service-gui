abe69b0c976480fad290e2400af4e8efdb435a29
mvlinowskv <46280152+mvlinowskv@users.noreply.github.com>
2022-06-21 21:10:53 +0200

    add link to add user form

--- src/components/AdminUsersManagement.vue
+++ src/components/AdminUsersManagement.vue
@@ -109,6 +109,17 @@
         <div
             class="bg-for-menu bg-main justify-content-center align-items-center d-flex flex-column gap-0 flex-md-row menu-fixed-bottom-inner gap-0 gap-md-2 gap-lg-3 px-4 w-max-content">
           <MenuComponent/>
+            <div class="d-flex flex-column justify-content-center">
+            <router-link to="/adding-user" class="bg-transparent mt-2">
+            <svg class="menu-fixed-button" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                <path
+                    d="M224 256c70.7 0 128-57.31 128-128S294.7 0 224 0C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3C0 496.5 15.52 512 34.66 512h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM616 200h-48v-48C568 138.8 557.3 128 544 128s-24 10.75-24 24v48h-48C458.8 200 448 210.8 448 224s10.75 24 24 24h48v48C520 309.3 530.8 320 544 320s24-10.75 24-24v-48h48C629.3 248 640 237.3 640 224S629.3 200 616 200z"/>
+            </svg>
+            </router-link>
+            <h2 class="font-small color-white font-weight-normal text-center "
+                style="font-size: 0.75rem; letter-spacing: 0.5px">Dodaj użytkownika</h2>
+        </div>
         </div>
       </section>
     </section>
