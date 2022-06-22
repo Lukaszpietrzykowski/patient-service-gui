c28cbf8d16a01610f5f6f94e3f7e2c44f51702d5
Łukasz Pietrzykowski <lukasz.pietrzykowski@execon.pl>
2022-06-22 21:15:59 +0200

    edycja departmentów

--- src/components/AdminWardManagement.vue
+++ src/components/AdminWardManagement.vue
@@ -34,7 +34,8 @@
                       d="M55.1 56.04C55.1 42.78 66.74 32.04 79.1 32.04H111.1C125.3 32.04 135.1 42.78 135.1 56.04V176H151.1C165.3 176 175.1 186.8 175.1 200C175.1 213.3 165.3 224 151.1 224H71.1C58.74 224 47.1 213.3 47.1 200C47.1 186.8 58.74 176 71.1 176H87.1V80.04H79.1C66.74 80.04 55.1 69.29 55.1 56.04V56.04zM118.7 341.2C112.1 333.8 100.4 334.3 94.65 342.4L83.53 357.9C75.83 368.7 60.84 371.2 50.05 363.5C39.26 355.8 36.77 340.8 44.47 330.1L55.59 314.5C79.33 281.2 127.9 278.8 154.8 309.6C176.1 333.1 175.6 370.5 153.7 394.3L118.8 432H152C165.3 432 176 442.7 176 456C176 469.3 165.3 480 152 480H64C54.47 480 45.84 474.4 42.02 465.6C38.19 456.9 39.9 446.7 46.36 439.7L118.4 361.7C123.7 355.9 123.8 347.1 118.7 341.2L118.7 341.2zM512 64C529.7 64 544 78.33 544 96C544 113.7 529.7 128 512 128H256C238.3 128 224 113.7 224 96C224 78.33 238.3 64 256 64H512zM512 224C529.7 224 544 238.3 544 256C544 273.7 529.7 288 512 288H256C238.3 288 224 273.7 224 256C224 238.3 238.3 224 256 224H512zM512 384C529.7 384 544 398.3 544 416C544 433.7 529.7 448 512 448H256C238.3 448 224 433.7 224 416C224 398.3 238.3 384 256 384H512z"/>
                 </svg>
                 <p class="m-0">Wszystkie łóżka <br>
-                  <input type="number" v-model="department.availableBeds" v-bind:min="department.occupiedBeds"></p>
+                  <input type="number" v-model="department.availableBeds" v-on:keyup="checkValue(department)" v-on:change="checkValue(department)"></p>
+                  <div v-if="!this.edit.isDataCorrect" id="tooltip">Elo</div>
               </div>
               <div v-on:click="disableEditMode"
                    class="d-flex flex-row justify-content-center action-button" style="gap:0.75rem">
@@ -50,10 +51,10 @@
                     style="font-size: 0.75rem; letter-spacing: 0.5px">Cofnij edycje</h2>
               </div>
 
-              <div v-on:click="submitEdit(department)"
+              <div v-on:click.prevent="this.edit.isDataCorrect && submitEdit(department)"
                    class="d-flex flex-row justify-content-center action-button" style="gap:0.75rem">
                 <a class="bg-transparent mt-2">
-                  <svg class="menu-admin-form-button" fill="#05a081" xmlns="http://www.w3.org/2000/svg"
+                  <svg id="arrow" class="menu-admin-form-button" fill="#05a081" xmlns="http://www.w3.org/2000/svg"
                        viewBox="0 0 576 512">
                     <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
                     <path
@@ -109,7 +110,7 @@
                     style="font-size: 0.75rem; letter-spacing: 0.5px">Edytuj oddział</h2>
               </div>
 
-              <div v-on:click="removeDepartment(department.id)"
+              <div  v-on:click="removeDepartment(department.id)"
                    class="d-flex flex-row justify-content-center action-button" style="gap:0.75rem">
                 <a class="bg-transparent mt-2">
                   <svg class="menu-admin-form-button" fill="#05a081" xmlns="http://www.w3.org/2000/svg"
@@ -163,6 +164,7 @@ export default {
   data() {
     return {
       edit: {
+        isDataCorrect: false,
         editMode: false,
         elementId: ''
       },
@@ -185,6 +187,17 @@ export default {
   },
 
   methods: {
+
+    checkValue(department) {
+      if (department.occupiedBeds > department.availableBeds) {
+        console.log( department.availableBeds)
+        this.edit.isDataCorrect = false;
+        document.getElementById('arrow').classList.add('szary');
+      } else {
+        this.edit.isDataCorrect = true;
+        document.getElementById('arrow').classList.remove('szary');
+      }
+    },
     submitEdit(department) {
       department.hospitalId = this.hospitalId;
       axios.put(`https://patient-service-api.herokuapp.com/department/update/${department.id}`, department)
@@ -199,11 +212,13 @@ export default {
     toggleEditMode(id) {
       this.edit.editMode = true;
       this.edit.elementId = id;
+      this.edit.isDataCorrect = true;
     },
 
     disableEditMode() {
       this.edit.editMode = false;
       this.edit.elementId = '';
+      this.edit.isDataCorrect = true;
     },
     submit() {
       const department = {
@@ -240,7 +255,7 @@ export default {
             }
           })
     }
-  }
+  },
 }
 
 </script>
@@ -248,6 +263,10 @@ export default {
 <style scoped>
 .action-button:hover {
   cursor: pointer;
+}
+
+.szary {
+  fill: gray;
 }
 
 </style>
